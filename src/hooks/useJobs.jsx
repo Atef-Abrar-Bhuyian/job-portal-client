6157bfe76da8a33b8077113a7689d833b56c1914
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-07 02:32:40 +0600

    sort btn and functionalities added

--- src/hooks/useJobs.jsx
+++ src/hooks/useJobs.jsx
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const useJobs = () => {
+const useJobs = (sort) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://job-protal-server-ten.vercel.app/jobs").then((res) => {
+    axios.get(`https://job-protal-server-ten.vercel.app/jobs?sort=${sort}`).then((res) => {
       setLoading(false);
       setJobs(res.data);
     });
-  }, []);
+  }, [sort]);
 
   return {jobs,loading};
 };
--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import useJobs from "../../hooks/useJobs";
 import HotJobCart from "../Home/HotJobCart";
 
 const AllJobs = () => {
-  const { jobs, loading } = useJobs();
+    const [sort, setSort] = useState(false);
+  const { jobs, loading } = useJobs(sort);
   if(loading){
     return <h2>Data Loading</h2>
   }
   return (
     <div>
       <h1 className="text-4xl text-center font-bold">All Jobs here</h1>
+      <div className="w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center">
+      <button onClick={()=>setSort(!sort)} className={`btn btn-neutral ${sort ? "btn-success": ""}`}>{sort ? "Sorted By Salary":"Sort By Salary"}</button>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {jobs.map((job) => (
           <HotJobCart key={job._id} job={job}></HotJobCart>
