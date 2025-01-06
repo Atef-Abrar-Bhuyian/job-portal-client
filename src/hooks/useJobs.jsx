a67bccfea2f6a9b2a96c2a54b81d2639f1d5ef53
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-01-07 03:08:02 +0600

    search functionalities done

--- src/hooks/useJobs.jsx
+++ src/hooks/useJobs.jsx
@@ -1,16 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const useJobs = (sort) => {
+const useJobs = (sort,search) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(`https://job-protal-server-ten.vercel.app/jobs?sort=${sort}`).then((res) => {
+    axios.get(`https://job-protal-server-ten.vercel.app/jobs?sort=${sort}&search=${search}`).then((res) => {
       setLoading(false);
       setJobs(res.data);
     });
-  }, [sort]);
+  }, [sort,search]);
 
   return {jobs,loading};
 };
--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -2,18 +2,34 @@
 import React, { useState } from "react";
 import useJobs from "../../hooks/useJobs";
 import HotJobCart from "../Home/HotJobCart";
+import { BiSearch } from "react-icons/bi";
 
 const AllJobs = () => {
-    const [sort, setSort] = useState(false);
-  const { jobs, loading } = useJobs(sort);
-  if(loading){
-    return <h2>Data Loading</h2>
+  const [sort, setSort] = useState(false);
+  const [search, setSearch] = useState("");
+  const { jobs, loading } = useJobs(sort, search);
+  if (loading) {
+    return <h2>Data Loading</h2>;
   }
   return (
     <div>
       <h1 className="text-4xl text-center font-bold">All Jobs here</h1>
-      <div className="w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center">
-      <button onClick={()=>setSort(!sort)} className={`btn btn-neutral ${sort ? "btn-success": ""}`}>{sort ? "Sorted By Salary":"Sort By Salary"}</button>
+      <div className="w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center gap-6 my-8">
+        <button
+          onClick={() => setSort(!sort)}
+          className={`btn btn-neutral ${sort ? "btn-success" : ""}`}
+        >
+          {sort ? "Sorted By Salary" : "Sort By Salary"}
+        </button>
+        <div className="flex items-center justify-center gap-2">
+          <BiSearch />
+          <input
+            onKeyUp={(e) => setSearch(e.target.value)}
+            type="text"
+            className="input w-full"
+            placeholder="Search Jobs By Location"
+          />
+        </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {jobs.map((job) => (
